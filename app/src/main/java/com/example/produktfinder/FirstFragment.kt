96b9ab55922fbb2bb569004d7e74b6611c543829
Yamin Eigenheer <yamin.eigenheer@hotmail.ch>
2023-11-23 16:20:26 +0100

    Bottom Navigation hinzugefügt

--- app/src/main/java/com/example/produktfinder/FirstFragment.kt
+++ app/src/main/java/com/example/produktfinder/FirstFragment.kt
@@ -32,10 +32,6 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-
         binding.buttonCoop.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_CoopFragment)
         }
--- app/src/main/java/com/example/produktfinder/MainActivity.kt
+++ app/src/main/java/com/example/produktfinder/MainActivity.kt
@@ -10,11 +10,14 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.produktfinder.databinding.ActivityMainBinding
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +27,12 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
+
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        binding.bottomNavigation.setupWithNavController(navController)
 
 
     }
