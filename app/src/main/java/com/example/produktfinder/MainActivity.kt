9def1443ec4a046db4b156cbea6addadf59b1e40
Yamin Eigenheer <yamin.eigenheer@hotmail.ch>
2023-11-25 11:55:55 +0100

    Menu and MainActivity

--- app/src/main/java/com/example/produktfinder/MainActivity.kt
+++ app/src/main/java/com/example/produktfinder/MainActivity.kt
@@ -64,12 +64,14 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+
+    //Function not needed, was for the settings button top left, which was assigned in the menu_main.xml
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            //R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
